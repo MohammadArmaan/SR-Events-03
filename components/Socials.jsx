eeb0ce370ab7cb316f70388c07afdbe774f98738
Mohammad Armaan <mohammadarmaan7786@Mohammads-Air.lan>
2025-07-12 15:29:57 +0530

    Altered Changes

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -6,24 +6,25 @@ import {
     RiFacebookFill,
     RiInstagramFill,
     RiWhatsappFill,
-    RiTwitterFill,
+
+    RiTwitterXFill,
 } from "react-icons/ri";
 
 const icons = [
-    { path: "/", name: <RiFacebookFill /> },
-    { path: "/", name: <RiInstagramFill /> },
-    { path: "/", name: <RiWhatsappFill /> },
-    { path: "/", name: <RiSnapchatFill /> },
-    { path: "/", name: <RiTwitterFill /> },
+    { path: "https://www.facebook.com/share/1BzxGZ2UwT/", name: <RiFacebookFill /> },
+    { path: "https://www.instagram.com/sr_portraits.events?igsh=bHppZWg3M3p4aHB5", name: <RiInstagramFill /> },
+    { path: "https://wa.link/20qt1s", name: <RiWhatsappFill /> },
+    { path: "https://www.snapchat.com/add/srportraits.eve?share_id=n50Yadrbl0A&locale=en-GB", name: <RiSnapchatFill /> },
+    { path: "https://x.com/Sr_Portraits?t=z-5wL4wDiiJEOPkBRd4YMA&s=09", name: <RiTwitterXFill /> },
 ];
 
 export default function Socials({ containerStyles, iconStyles }) {
     return (
         <div className={`${containerStyles}`}>
             {icons.map((icon, index) => (
-                <Link href={icon.path} key={index}>
+                <a href={icon.path} key={index} target="_blank">
                     <div className={`${iconStyles}`}>{icon.name}</div>
-                </Link>
+                </a>
             ))}
         </div>
     );
